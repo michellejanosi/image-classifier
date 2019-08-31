3b711e44882a66793ef9c21aeb1f815ed5969bbc
michellejanosi <michelle.janosi@gmail.com>
2019-08-31 16:45:53 -0400

    Set up MobileNet for inference in browser

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("loaded")
+let net;
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model
+  net = await mobilenet.load();
+  console.log('Sucessfully loaded model');
+
+  // Make a prediction through the model on the image
+  const imgEl = document.getElementById('img');
+  const result = await net.classify(imgEl);
+  console.log(result);
+}
+
+app();
